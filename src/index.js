d822f6d127f63c96fbd52ef58b51db8061068b2d
error696a <hoangnguyen789a@gmail.com>
2021-01-21 12:06:48 +0700

    add expressJS, nodemon, morgan, bootstrap, static file and scss

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars  = require('express-handlebars');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
